9c844d50e3cbdb4f5e0604a5f8899d07f3316de6
Daniel8529 <daniel_montero@ucne.edu.do>
2022-11-09 18:16:35 -0500

    removiendo room

--- app/src/main/java/com/ucne/parcial2_daniel/di/AppModule.kt
+++ app/src/main/java/com/ucne/parcial2_daniel/di/AppModule.kt
@@ -10,26 +10,26 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-@Module
-@InstallIn(SingletonComponent::class)
-object AppModule {
-    @Singleton
-    @Provides
-    fun provideMoshi(): Moshi {
-        return Moshi
-            .Builder()
-            .add(KotlinJsonAdapterFactory())
-            .build()
-    }
-
-    @Singleton
-    @Provides
-    fun provideSagApi(moshi: Moshi):  {
-        return Retrofit.Builder()
-            .baseUrl()
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .build()
-            .create(::class.java)
-
-    }
-}
+//@Module
+//@InstallIn(SingletonComponent::class)
+//object AppModule {
+//    @Singleton
+//    @Provides
+//    fun provideMoshi(): Moshi {
+//        return Moshi
+//            .Builder()
+//            .add(KotlinJsonAdapterFactory())
+//            .build()
+//    }
+//
+//    @Singleton
+//    @Provides
+//    fun provideSagApi(moshi: Moshi):  {
+//        return Retrofit.Builder()
+//            .baseUrl()
+//            .addConverterFactory(MoshiConverterFactory.create(moshi))
+//            .build()
+//            .create(::class.java)
+//
+//    }
+//}
